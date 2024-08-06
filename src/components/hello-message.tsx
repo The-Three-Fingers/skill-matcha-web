47f70fe3eb4c76478d9dc0f69e8179b0b8ad7117
Sergei Ponomarev <sergei.ponomarev@scompler.com>
2024-08-06 09:30:30 +0400

    fix: fix imports

--- src/components/hello-message.tsx
+++ src/components/hello-message.tsx
@@ -1,7 +1,7 @@
 import { currentUser } from '@clerk/nextjs/server';
 import { getTranslations } from 'next-intl/server';
 
-const Hello = async () => {
+const HelloMessage = async () => {
   const t = await getTranslations('Dashboard');
   const user = await currentUser();
 
@@ -12,4 +12,4 @@ const Hello = async () => {
   );
 };
 
-export { Hello };
+export { HelloMessage };
