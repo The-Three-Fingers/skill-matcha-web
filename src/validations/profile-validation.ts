a154595007910a9ff0ff3c72eb310750cc81c53d
Sergei Ponomarev <sponom.dev@gmail.com>
2024-08-15 17:23:47 +0500

    Add profile schema (#14)
    Co-authored-by: Sergei Ponomarev <sergei.ponomarev@scompler.com>

--- src/validations/profile-validation.ts
+++ src/validations/profile-validation.ts
@@ -1,14 +1,56 @@
 import { z } from 'zod';
 
-// TODO add translations for error messages
-
-export const CreateProfileFormValidation = z.object({
-  name: z.string().min(1, '').max(150, {
-    message: '',
-  }),
-  lastName: z.string().min(1, '').max(150, {
-    message: '',
-  }),
-  roles: z.array(z.string()),
-  searchRoles: z.array(z.string()),
+export const ProfileValidation = z.object({
+  // Personal info
+  name: z
+    .string()
+    .min(1, {
+      message: 'Name must be at least 1 character',
+    })
+    .max(150, {
+      message: 'Last name must be maximum 150 characters',
+    }),
+  lastName: z
+    .string()
+    .min(1, {
+      message: 'Last name must be at least 1 character',
+    })
+    .max(150, {
+      message: 'Last name must be maximum 150 characters',
+    }),
+  avatarURL: z.string().optional(),
+  aboutInfo: z.string().max(1000).min(10).optional(),
+  languages: z.array(z.string().min(2).max(5)).default([]),
+  location: z.string().min(2).max(2).optional(),
+  // Role
+  roles: z
+    .array(z.string())
+    .min(1, {
+      message: 'Please choose one role',
+    })
+    .max(1),
+  subRoles: z.array(z.string()).max(5).default([]),
+  services: z
+    .array(z.string())
+    .min(1, {
+      message: 'At least one item is required',
+    })
+    .max(10),
+  // Idea
+  idea: z
+    .object({
+      stage: z.string().min(1, {
+        message: 'Idea stage must be selected',
+      }),
+      area: z.string().min(1, {
+        message: 'Idea area must be selected',
+      }),
+    })
+    .optional(),
+  // Search preferences
+  searchRoles: z.array(z.string()).default([]),
+  searchSubRoles: z.array(z.string()).max(5).default([]),
+  searchServices: z.array(z.string()).max(10).default([]),
+  // Others
+  availabilityTime: z.number().min(0).optional(),
 });
