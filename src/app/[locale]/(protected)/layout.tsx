74bc7b209f57d5701c6810ff45fc169314373bfe
Sergei Ponomarev <sergei.ponomarev@scompler.com>
2024-08-18 19:04:55 +0400

    fix: fix redirect

--- src/app/[locale]/(protected)/layout.tsx
+++ src/app/[locale]/(protected)/layout.tsx
@@ -4,7 +4,7 @@ import { Env } from '@/libs/Env';
 import { BaseTemplate } from '@/templates/base-template';
 
 export default async function Layout(props: { children: React.ReactNode }) {
-  if (Env.NODE_ENV === 'development') {
+  if (Env.NODE_ENV !== 'development') {
     redirect('/');
   }
 
