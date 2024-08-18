2c82603efb4e18687372360ed187c4ef4b915780
Sergei Ponomarev <sponom.dev@gmail.com>
2024-08-18 20:07:17 +0500

    Add redirects (#18)
    Co-authored-by: Sergei Ponomarev <sergei.ponomarev@scompler.com>

--- src/app/[locale]/(protected)/layout.tsx
+++ src/app/[locale]/(protected)/layout.tsx
@@ -1,6 +1,13 @@
+import { redirect } from 'next/navigation';
+
+import { Env } from '@/libs/Env';
 import { BaseTemplate } from '@/templates/base-template';
 
 export default async function Layout(props: { children: React.ReactNode }) {
+  if (Env.NODE_ENV !== 'development') {
+    redirect('/');
+  }
+
   return <BaseTemplate>{props.children}</BaseTemplate>;
 }
 
