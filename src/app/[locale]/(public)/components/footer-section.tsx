cc7cd3b89e1e29b363f0f435d888e302b54e02e2
TatianaP <tanya.email4work@gmail.com>
2024-08-21 17:51:38 +0400

    fix: comment PP and ToS links

--- src/app/[locale]/(public)/components/footer-section.tsx
+++ src/app/[locale]/(public)/components/footer-section.tsx
@@ -1,11 +1,10 @@
 import { Linkedin } from 'lucide-react';
-import Link from 'next/link';
-import { getTranslations } from 'next-intl/server';
 
+// import { getTranslations } from 'next-intl/server';
 import { TypographyH4 } from '@/components/ui/typography';
 
 export default async function FooterSection() {
-  const t = await getTranslations('externalLinks');
+  // const t = await getTranslations('externalLinks');
 
   const year = new Date().getFullYear();
 
@@ -13,13 +12,14 @@ export default async function FooterSection() {
     <footer className="flex flex-col items-center justify-center gap-8 p-10 text-center text-foreground lg:pt-14">
       <div className="flex flex-col gap-2">
         <TypographyH4>© {year} SKILLMATCHA</TypographyH4>
-        <Link href="/" className="hover:text-primary-500 transition">
+
+        {/* <Link href="/" className="hover:text-primary-500 transition">
           {t('termsOfService')}
         </Link>
 
         <Link href="/" className="hover:text-primary-500 transition">
           {t('privacyPolicy')}
-        </Link>
+        </Link> */}
       </div>
 
       <div className="flex justify-center">
