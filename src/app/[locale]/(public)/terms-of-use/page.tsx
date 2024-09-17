3bc384d3abce681f0ec83e6316b7bd9492d199a7
TatianaP <tanya.email4work@gmail.com>
2024-09-17 18:45:50 +0400

    fix: lint errors

--- src/app/[locale]/(public)/terms-of-use/page.tsx
+++ src/app/[locale]/(public)/terms-of-use/page.tsx
@@ -1,4 +1,5 @@
 import { getTranslations } from 'next-intl/server';
+
 import { TermsOfUse } from './terms-of-use';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
